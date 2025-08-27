cf9ab823c5d5777b41791a9c68b61032659a6105
Hari <43692908+hariscodebase@users.noreply.github.com>
2025-08-27 12:41:54 -0400

    Update index.js
    Correct the import library namespace

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Bugsnag from '@bugsnag'
+import Bugsnag from '@bugsnag/js'
 import BugsnagPluginReact from '@bugsnag/plugin-react'
 
 Bugsnag.start({
